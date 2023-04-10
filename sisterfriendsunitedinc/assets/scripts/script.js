e8ca44870fe9784c67706879cd34fda5fd5a4ea2
techlikeagirl <aewoodberry@ualr.edu>
2023-04-10 22:10:13 +0000

    update

--- sisterfriendsunitedinc/assets/scripts/script.js
+++ sisterfriendsunitedinc/assets/scripts/script.js
@@ -1,3 +1,4 @@
-const popover = new bootstrap.Popover('.example-popover', {
-    container: 'body'
-  })
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-J4S07L83GZ');
